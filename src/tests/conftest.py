8565cd1b9c132fa9ba70aa965328dbcbcf65b66c
Xander Harris <xander.harris@databricks.com>
2023-04-25 17:56:35 -0700

    Update first test
    9-add-new-versions-for-installs-via-pipenv
    Closes #9
    
    Changelog: changed

---
+++ src/tests/conftest.py
@@ -0,0 +1,7 @@
+"""Test configuration package."""
+from pathlib import Path
+import sys
+
+repo_root=Path(__file__).parent
+
+sys.path.append(repo_root)
---
+++ src/tests/test_youtube.py
@@ -0,0 +1,7 @@
+"""Test module for youtube package."""
+from sphinxcontrib.yt import youtube
+
+def test_youtube_class():
+    """Validate youtube class."""
+    test_youtube = youtube.youtube()
+    assert isinstance(test_youtube, youtube.youtube)
