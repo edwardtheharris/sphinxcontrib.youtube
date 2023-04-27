abb83ad9a35bed194e6098f8b5290a951c964d15
Xander Harris <xander.harris@databricks.com>
2023-04-26 17:16:15 -0700

    Update extensions, conf
    See #11
    
    Changelog: changed

--- conf.py
+++ conf.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python3
-# pylint: disable=invalid-name
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# pylint: disable=invalid-name,redefined-builtin
+"""Configuration file for the Sphinx documentation builder.
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+For the full list of built-in configuration values, see the
+[documentation](https://www.sphinx-doc.org/en/master/usage/configuration.html).
 
-project = 'Sphinx Contrib YouTube'
-copyright = '2023, Xander Harris'
+```{rubric} Project information
+```
+
+[Project information](https://tinyurl.com/255t89n7)
+"""
+from pathlib import Path
+
 author = 'Xander Harris'
-release = '0.2.3'
+copyright = '2023, The Internet'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+#: -- General configuration ---------------------------------------------------
+project = 'Sphinx Contrib YouTube'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+with Path('version').open('r', encoding='utf-8') as v_fh:
+    release = v_fh.read()
 
-extensions = []
+#: [general configuration](https://tinyurl.com/4vys37ez)
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.githubpages"
+]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+#: -- Options for HTML output -------------------------------------------------
 html_theme = 'alabaster'
+#: [html options](https://tinyurl.com/35zdr2a7)
 html_static_path = ['_static']
